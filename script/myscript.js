01d0f6f42518dcc588e8e8ac1e46970c05a7b562
Andrea Parisi <andreaparisi4502@gmail.com>
2021-11-09 16:42:36 +0100

    commit exercise

---
+++ script/myscript.js
@@ -0,0 +1,42 @@
+let numeri = [];
+
+for(let i = 0; i < 5; i++){
+    numeri[i] = randomNum();
+}
+let container = document.querySelector(".container");
+container.innerHTML += numeri;
+
+let timer = document.querySelector(".timer");
+let time = 5;
+
+let clock = setInterval(() => {
+    time--;
+    timer.innerHTML = time;
+    if(time === 0){
+        clearInterval(clock);
+        container.innerHTML = `Numeri indovinati: ${numeriIndovinati}`;
+    }
+}, 1000);
+
+
+let numeriIpotesi = []; 
+for(let i = 0; i < 5; i++){
+    numeriIpotesi[i]= parseInt(prompt("Indovina i numeri: "));
+}
+
+let numeriIndovinati = [];
+for(let i = 0; i < numeri.length; i++){
+    if((numeri.includes(numeriIpotesi[i]))){
+        numeriIndovinati.push(numeriIpotesi[i]);
+    }
+}
+container.innerHTML += numeriIndovinati;
+
+//funzioni
+function randomNum(){
+    let numero = Math.floor(Math.random() * 100 + 1);
+    if (!(numeri.includes(numero))){
+        numeri.push(numero);
+    }
+    return numero;
+}
